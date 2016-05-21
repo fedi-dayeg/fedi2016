cb0b0655fc1507713c9ded64939db6c5e4e4d8eb
fedi dayeg <fedi dayeg>
2016-05-21 12:51:52 +0100

    index.html

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 function Commencer()
 {
-    var canvas=document.getElementById("mainCanvas");
+    var canvas=document.getElementById("moncanvas");
     //attribuer un context
     var Moncontext= canvas.getElementById("2d");
     // canvas temps
@@ -16,4 +16,19 @@ function Commencer()
     Moncontext2.font = "bold 25px helvetica";
     //fillText permet de colorer le compteur de temps
     Moncontext2.fillText(temp, 225, 20);
+
+    //variable pour les rectangle de joueur
+    var longeur=500, largeur=400, vitesse=3;
+    //variable de score
+    var score=0;
+    //variable pour le touche de mouvement
+    var touche=[];
+    //une enregestrement pour le joueur
+    var joueur={
+      x: 40,
+      y: 40,
+      longeur: 20,
+      largeur: 20
+    };
+
 }
